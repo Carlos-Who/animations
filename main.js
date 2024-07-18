0185fb66d39b3f012948a331759bdf09c3f89d1d
Carlos Díaz <carlosdiaz.hello@gmail.com>
2024-07-17 23:01:13 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+import { gsap } from "gsap";
+
+let flag = false;
+let cardTimeLine = gsap.timeline({paused: true});
+cardTimeLine.to(".faces", {rotationY:180, duration:1, ease:"power1.inOut"})
+
+const cardFront =  document.querySelector('.front');
+const cardBack =  document.querySelector('.back');
+
+cardFront.addEventListener("click", () => {
+    console.log("Click front card")
+    cardTimeLine.play();
+});
+
+cardBack.addEventListener("click", () => {
+    console.log("Click back card")
+    cardTimeLine.reverse();
+});
+
+document.querySelector('#app').innerHTML = `
+<!--
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+  -->
+
+`
+
+setupCounter(document.querySelector('#counter'))
