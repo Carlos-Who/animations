af19821bc95f801e14c204907289ac8cf6e8743f
Carlos Díaz <carlosdiaz.hello@gmail.com>
2024-07-30 22:51:25 -0600

    expanders mejorados

--- main.js
+++ main.js
@@ -6,32 +6,48 @@ import { gsap } from "gsap";
 
 let active;
 let expanders = gsap.utils.toArray(".expander");
+gsap.set(".expander", { backgroundColor:gsap.utils.wrap(["#987408", "#880570", "#05606a"])});
+console.log("--------------------")
+console.log(expanders);
+console.log("--------------------")
 
-expanders.forEach(function(expander, index){
-    let close = expander.querySelector(".close")
-    let animation = gsap.timeline({paused:true})
-    animation.to(expander, {width:250, duration:0.4})
-        .from(close, {opacity:0, scale:0.4, duration:0.1, x:"-=10"}, "-=0.1")
-    expander.animation = animation // assign the animation to the current element
+let close = document.querySelector(".close");
+console.log(close);
+
+expanders.forEach((expander, index) => {
+    console.log(index);
+    // let close = expander.querySelector(".close");
+    // console.log(close);
+    let animation = gsap.timeline({paused:true});
+
+    animation.to(expander, {
+        width:250,
+        duration:0.4
+    }).from(close, {
+        opacity:0,
+        scale:0.4,
+        duration:0.1,
+        x:"-=10"
+    }, "-=0.1");
+
+    expander.animation = animation; // assign the animation to the current element
 
     expander.addEventListener("click", function(){
-
         if(active){
-            active.animation.reverse() // reverse (close) active element's animation
+            active.animation.reverse(); // reverse (close) active element's animation
         }
-
-        expander.animation.play() // play (open) the clicked element's animation
-        active = expander
+        expander.animation.play(); // play (open) the clicked element's animation
+        active = expander;
     })
 
     close.addEventListener("click", function(event){
-        event.stopPropagation()
-        expander.animation.reverse()
-    })
+        event.stopPropagation();
+        expander.animation.reverse();
+    });
 
-    console.log(expander)
+    console.log(expander);
 
-})
+});
 
-gsap.set(".expander", {backgroundColor:gsap.utils.wrap(["#f5ce5b", "#c570b6", "#78d6e0"])})
+
 
